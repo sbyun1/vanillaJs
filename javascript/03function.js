441fafe2723e0b740c21b7d05c8ab435a351ebec
Noa <noasueden@gmail.com>
2021-11-22 08:53:31 +0900

    04class complete

--- javascript/03function.js
+++ javascript/03function.js
@@ -177,3 +177,20 @@ hello1();
 // Quiz !
 // function calculate(command, a, b)
 // command : add, substract, divide, multiply, remainder
+function calculate(command) {
+  switch (comamnd) {
+    case 'add':
+      return a + b;
+    case 'substract':
+      return a - b;
+    case 'divide':
+      return a / b;
+    case 'multiply':
+      return a * b;
+    case 'remainder':
+      return a % b;
+    default:
+      throw Error('unknown command');
+  }
+}
+console.log(calculate('add', 2, 3));
