a06c37dbf468c6914d22656f3b7fe54e96259dde
Subin Byun <sbyun95@gmail.com>
2022-01-21 12:49:32 +0900

    0121: variable and boolean

--- lena/momentum/app.js
+++ lena/momentum/app.js
@@ -23,11 +23,14 @@ console.log("hello " + myName);
 myName = "subin";
 console.log("korean name is " + myName);
 
+//------------------------------------------
 
+//2. booleans : null, undefined, true, false
 
-//2. booleans
-// null: nothing
-// undefined: variable exists but have no value
+// null: nothing: never happens automatically
+// undefined: variable exists in memory but have no value
 const amIFat = null;
 let something; 
 console.log(something, amIFat);
+//------------------------------------------
+
