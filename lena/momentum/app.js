25cc04bd8d2bbef8ae5322b01eaf658e9bfacab2
Subin Byun <sbyun95@gmail.com>
2022-01-21 12:43:57 +0900

    0121: variable and boolean

--- lena/momentum/app.js
+++ lena/momentum/app.js
@@ -1,1 +1,33 @@
-alert('hi');
+//1. variables
+
+//const: non-changeable
+// *** normally use 'const' by default****
+const a = 5;
+const b = 2;
+
+// let: changeable
+let myName = "lena";
+
+// var: old way of creating variable, could be changed anywhere
+
+// if create with 'const' and then later try to change, 
+// it will show and error: 'const' cannot be changed
+
+//conventional way name a variable in JS:camel case
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+myName = "subin";
+console.log("korean name is " + myName);
+
+
+
+//2. booleans
+// null: nothing
+// undefined: variable exists but have no value
+const amIFat = null;
+let something; 
+console.log(something, amIFat);
