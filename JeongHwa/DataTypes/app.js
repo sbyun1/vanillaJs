4658255ddf12b95c6eba65be43e25e73eb2b9233
GongJeongHwa <ggongjh@naver.com>
2022-01-24 01:20:51 +0900

    2.0 - 2.3 commit

---
+++ JeongHwa/DataTypes/app.js
@@ -0,0 +1,13 @@
+
+//sadfasf; 와 같이 js가 이해하지 못하는
+
+2+2 //정수(Integer) + 정수 가능
+
+1.5+1.5 //실수(float) + 실수 가능
+
+656656+2.5 //정수(Integer) + 실수(float) 가능
+
+//문자를 입력할땐 ""을 사용해야한다
+"hello"
+"javascript"
+"Jeong"+"Hwa" //문자(string) + 문자 가능
