1850c1c3ecd0d7ac78b8f03529f9bb9e8444e245
Subin Byun <sbyun95@gmail.com>
2022-01-25 01:44:21 +0900

    0125: 4.2 Events

--- lena/momentum/app.js
+++ lena/momentum/app.js
@@ -1,1 +1,17 @@
-//test
+//const loginForm = document.getElementById("login-form");
+                            // querySelector("#login-form")
+//const loginInput = loginForm.querySelector("input");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(event){
+    event.preventDefault(); //stops default behavior of any event
+    console.log(loginInput.value);
+    //const userName = loginInput.value;
+    //console.log(userName);
+}
+
+//on submit - validation 
+loginForm.addEventListener("submit", onLoginSubmit) 
+//when using EventListener, after JS calls the function,
+//it will return information about the event that just happened
