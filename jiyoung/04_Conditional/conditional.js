00bbc3543f55c2a8b2a6139290db12ac7ce83385
jiyoungbb <jiiyoung0906@naver.com>
2022-01-25 11:43:10 +0900

    2.14 conditional part two

--- jiyoung/04_Conditional/conditional.js
+++ jiyoung/04_Conditional/conditional.js
@@ -1,6 +1,6 @@
 //prompt(): 아주 오래된 방법으로 잘 사용하지 않음
 //css 적용 안 되고 답이 입력될 때까지 js 실행을 일시적으로 멈추기 때문
-const age = prompt("How old are you?");
+const age = parseInt(prompt("How old are you?"));
 
 //typeof: 변수 타입 보는 방법
 //prompt()에서 string이 default값이기 때문에 숫자를 입력해도 타임은 string으로 나옴
@@ -14,3 +14,12 @@ console.log(typeof "15", typeof parseInt("15"));
 const ageOfForeigner = prompt("국제 나이로 몇 살?");
 const krAge = parseInt(ageOfForeigner) + 2;
 console.log("당신의 국제 나이는 " + ageOfForeigner + "이고, 한국 나이는 " + krAge + "입니다.");
+
+//isNaN(): 변수 값이 NaN인지 아닌지를 boolean으로 판변해주는 함수
+console.log(isNaN(age));
+
+if(isNaN(age)){
+    console.log("Please write number");
+}else{
+    console.log("Thank you for writing your age");
+}
