4384449fbec5a89df53c30443e29dbfb65b992e4
siren0416 <78391171+siren0416@users.noreply.github.com>
2022-01-25 12:40:44 +0900

    Array update

--- Sangho/app.js
+++ Sangho/app.js
@@ -1,8 +1,41 @@
-let a = 5; // const > 상수 (변하지 않는 수)
+// const > 상수 (변하지 않는 수)
+// let > 변할 수 있는 변수 선언
+const a = 5; 
 const b = 2;
-const myName = "sangho";
+let myName = "sangho";
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
-console.log("hello "+ myName)
+console.log("hello "+ myName);
+
+myName = "sanggo"; // let으로 하는 것을 업데이트를 통해 수정
+
+console.log("your new name is "+ myName);
+
+
+// Booleans
+const amIFat = false;
+let something;
+const amIFat2 = null;
+
+console.log(amIFat);
+console.log(amIFat2);
+
+
+// Arrays
+const days0fWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+const nonsense = [1, 2, "hello", false, null, true, undefined, "sangho"]
+
+// get Item from Array
+console.log(days0fWeek);
+
+// Add one more day to the array
+days0fWeek.push("sun");
+
+console.log(days0fWeek);
+
+
+
+
+
