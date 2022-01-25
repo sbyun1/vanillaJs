d0a5a2d858d14677c8777750280f3a4169eb83ad
yeji <yedoong919@gmail.com>
2022-01-26 00:45:14 +0900

    part2_DataTypes

---
+++ yeji/Part2_BasicJS/2.1_DataTypes/app.js
@@ -0,0 +1,28 @@
+/* Data Types */
+// 1. Integer (정수)
+// 2. float (실수)
+// 3. String (문자열)
+
+// console에 2+2를 작성하면 4를 반환해줌 -> JS는 2와 +를 작성하면 무엇을 해야할 지 안다는 것
+2+2
+//-> result : 4
+
+// integer + float 가능
+65555554+2.5
+//-> result : 65555556.5
+
+// hello 만 작성 시, error 발생 -> "hello is not defined ~~(정의되어 있지 않다)"
+// hello;
+//-> error : hello is undefined (= has no value)
+// => hello라는 것은 만들어지지 않았고, 값이 존재하지 않는다는 뜻
+
+// 그렇다면 String을 작성하려면 ?
+// "Double Quotation" 안에 String 작성
+"hello";
+
+// String + String
+"hello " + "my name is yeji"
+//-> result : "hello my name is yeji"
+
+"ye" + "ji"
+//-> result : "yeji"
