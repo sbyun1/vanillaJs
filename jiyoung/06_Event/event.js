9d8b5741332ac414ace9a75cca47e0ba5b8f2de4
jiyoungbb <jiiyoung0906@naver.com>
2022-01-26 12:01:10 +0900

    3.4 events part two

--- jiyoung/06_Event/event.js
+++ jiyoung/06_Event/event.js
@@ -4,7 +4,17 @@ function handleTitleClick(){
     title.style.color = "blue";
 }
 
+function handleMouseEnter(){
+    title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone";
+}
+
 /* addEventListner */
 //클릭 이벤트 발생 시 handleTitleClick function이 동작하길 원함
 //handleTitleClick();이 아닌 이유: ()가 있으면 실행버튼 클릭하면 작동하는데 클릭 이벤트가 발생했을 때만 작동하길 원하기 때문
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
