9633fc14988c44ac4fc6bc8d78bd24c291752461
Noa <noasueden@gmail.com>
2022-01-28 20:50:15 +0900

    ...

--- noa/momentum/app.js
+++ noa/momentum/app.js
@@ -1,8 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
-const h1 = document.querySelector("div.hello:first-child h1");
+function onLoginSubmit(event) {
+  const username = loginInput.value;
+  /*
+  form이 아닌 div에 넣을 경우
+  if (username === "") {
+    alert("Please write your name.");
+  } else if (username.length > 15) {
+    alert("Your name is too long.");
+  } 
+  */
 
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
+  //stop refresh when submit event happened
+  //preventDefault() : stops the default behavior of any event
+  event.preventDefault();
+  console.log(username);
+
 }
 
-h1.addEventListener("click", handleTitleClick);
+/*
+function handleLinkClick(event) {
+  event.preventDefault();
+  alert("clicked!");
+}
+*/
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
