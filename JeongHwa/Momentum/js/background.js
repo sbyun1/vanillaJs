666bb5318969f06c6111a48a2992bc4e1e53885f
GongJeongHwa <ggongjh@naver.com>
2022-01-29 21:46:03 +0900

    2022.01.29 / 6.0 - 6.2

---
+++ JeongHwa/Momentum/js/background.js
@@ -0,0 +1,11 @@
+const images = ["0.jpg", "1.jpg", "2.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img"); //img태그 생성
+
+bgImage.src = `img/${chosenImage}`;
+
+console.log(bgImage); //random 이미지 삽입
+
+document.body.appendChild(bgImage); //bgImage html의 body에 추가
